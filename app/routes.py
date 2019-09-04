2a72e23aec59d57df3690db073b5caba0f28f3e9
Garrett <garrettstillwater@gmail.com>
2019-09-03 21:32:20 -0500

    Updates to initialize js data to values instead of nothing

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from app import app
 from flask import render_template
 from datetime import datetime
 
+numbers = [1,2,3,4,5,6,7,8,9]
+
 @app.route('/')
 def hello_world():
     message = {'name':'Pos3d',
@@ -11,4 +13,19 @@ def hello_world():
 @app.route('/index')
 def ind():
    print("Index")
-   return render_template('index.html')
+   global numbers
+   i=0
+   leng = len(numbers)
+   if len(numbers) > 0:
+      numb = "var numbers_received = ["
+      for number in numbers:
+         i += 1
+         numb += str(number)
+         if i is not leng:
+            numb += ", "
+         # end if
+      # end for
+      numb += "]\n"
+   else:
+      numb = "var numbers_received = [];"
+   return render_template('index.html',numb=numb)
--- app/static/js/application.js
+++ app/static/js/application.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
     //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-    var numbers_received = [];
 
     //receive details from server
     socket.on('newnumber', function(msg) {
